796a8b9da41e87e0a0324992284f43e785655d2d
hadi <hadi@depository.id>
2024-02-10 00:36:38 +0700

    create block

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    uint64
+	transactions []string
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp:		%d\n", b.timestamp)
+	fmt.Printf("nonce:			%d\n", b.nonce)
+	fmt.Printf("previousHash:		%s\n", b.previousHash)
+	fmt.Printf("transactions:		%s\n", b.transactions)
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	return &Block{
+		nonce:        nonce,
+		previousHash: previousHash,
+		timestamp:    uint64(time.Now().UnixNano()),
+	}
+}
 
 func main() {
-	log.SetPrefix("Blockchain: ")
-	log.Println("test")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
